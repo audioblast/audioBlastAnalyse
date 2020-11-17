ce3e31efbe792ed79e6d4be0019be0c55fdccc21
Ed Baker <edwbaker@gmail.com>
2020-11-17 21:40:29 +0000

    tmp file names.

--- R/analyse.R
+++ R/analyse.R
@@ -7,10 +7,11 @@ analyse <- function(db, force=FALSE) {
   res <- dbSendQuery(db, "SELECT source, id, `file`, `type`, Duration FROM `audioblast`.`recordings`")
   ss <- dbFetch(res)
   dbClearResult(res)
-  tmp <- tempfile()
 
   for (i in 1:nrow(ss)) {
+    tmp <- tempfile()
     a_tdsc(db, ss[[i, "source"]], ss[[i, "id"]], ss[[i, "file"]], ss[[i, "type"]], ss[[i, "Duration"]], tmp, force)
+    unlink(tmp)
   }
-  unlink(tmp)
+
 }
