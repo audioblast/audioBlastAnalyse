68b45705dfa3b6b862b5252915db1b2bdb82027b
Ed Baker <edwbaker@gmail.com>
2020-11-18 12:59:33 +0000

    db utils.

--- R/db_utils.R
+++ R/db_utils.R
@@ -1,27 +1,44 @@
 deleteAnalysis <- function(db, table, source, id){
-  dbExecute(db, paste0("DELETE FROM `",table,"` WHERE source = '",source,"' AND id=",id))
+  tryCatch({
+    dbExecute(db, paste0("DELETE FROM `",table,"` WHERE source = '",source,"' AND id=",id))
+  }, error = function(e) {
+    print(e)
+  })
 }
 
 analysedRowCount <- function(db, table, source, id){
-  sql <- paste0("SELECT `source`, `id` FROM `", table, "` WHERE `source` = '",source,"' AND id=",id)
-  res <- dbSendQuery(db, sql)
-  dbFetch(res)
-  return(dbGetRowCount(res))
+  tryCatch({
+    sql <- paste0("SELECT `source`, `id` FROM `", table, "` WHERE `source` = '",source,"' AND id=",id)
+    res <- dbSendQuery(db, sql)
+    dbFetch(res)
+    return(dbGetRowCount(res))
+  }, error = function(e) {
+    print(e)
+  })
 }
 
 rowAnalysed <- function(db, table, source, id, startTime){
-  sql <- paste0("SELECT `source`, `id` FROM `",table,"` WHERE `source` = '",source,"' AND id=",id," AND startTime = ",startTime)
-  print(sql)
-  res <- dbSendQuery(db, sql)
-  dbFetch(res)
-  if (dbGetRowCount(res) != 0) {
-    return(TRUE)
-  } else {
-    return(FALSE)
-  }
+  tryCatch({
+    sql <- paste0("SELECT `source`, `id` FROM `",table,"` WHERE `source` = '",source,"' AND id=",id," AND startTime = ",startTime)
+    print(sql)
+    res <- dbSendQuery(db, sql)
+    dbFetch(res)
+    if (dbGetRowCount(res) != 0) {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  }, error = function(e) {
+    print(e)
+  })
 }
 
 insertAnalysis <- function(db, table, source, id, duration, startTime, result){
-  sql <- paste0("INSERT INTO `", table, "` VALUES ('",source,"', '",id,"', '", duration, "', ",startTime,", '", result,"')")
-  dbExecute(db, sql)
+  tryCatch({
+    sql <- paste0("INSERT INTO `", table, "` VALUES ('",source,"', '",id,"', '", duration, "', ",startTime,", '", result,"')")
+    dbExecute(db, sql)
+  }, error = function(e) {
+    print(e)
+  })
 }
+
