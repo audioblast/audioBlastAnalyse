49c6f8ad3a85245a2d4a53327cf7bec80bab7a22
Ed Baker <edwbaker@gmail.com>
2020-11-18 14:05:09 +0000

    Use system curl

--- R/dl_file.R
+++ R/dl_file.R
@@ -1,6 +1,5 @@
-#' @importFrom curl curl_download new_handle
 dl_file <- function(file, tmp) {
   if(!file.exists(tmp)){
-    curl_download(file, tmp, handle=new_handle(verbose = TRUE))
+    system(paste("curl -o",tmp," --retry 5", file))
   }
 }
