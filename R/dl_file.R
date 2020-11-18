ed84568ab9b2127f851787d4c5ca989cb9653a61
Ed Baker <edwbaker@gmail.com>
2020-11-18 13:30:12 +0000

    Debugging.

--- R/dl_file.R
+++ R/dl_file.R
@@ -1,6 +1,6 @@
 #' @importFrom utils download.file
 dl_file <- function(file, tmp) {
   if(!file.exists(tmp)){
-    curl_download(file, tmp)
+    curl_download(file, tmp, handle=new_handle(verbose = TRUE))
   }
 }
