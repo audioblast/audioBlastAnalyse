7e53bd9b6456cd03648074e8851b5bc98cb9caa8
Ed Baker <edwbaker@gmail.com>
2020-11-20 12:41:51 +0000

    More gressive retry on file download.

--- R/dl_file.R
+++ R/dl_file.R
@@ -1,5 +1,5 @@
 dl_file <- function(file, tmp) {
   if(!file.exists(tmp)){
-    system(paste("wget --retry-connrefused --tries=100 -O",tmp, file))
+    system(paste("wget --retry-connrefused --waitretry=0 --tries=1000 -O",tmp, file))
   }
 }
