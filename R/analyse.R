c95068c17a89ee7e2543c8ef4af67c95d883276c
Ed Baker <edwbaker@gmail.com>
2020-11-22 13:34:48 +0000

    Verbose mode.

--- R/analyse.R
+++ R/analyse.R
@@ -2,9 +2,10 @@
 #'
 #' @param db database connector
 #' @param force If TRUE recalculates all values
+#' @param verbose If TRUE outputs debugging information
 #' @importFrom tools file_ext
 #' @export
-analyse <- function(db, force=FALSE) {
+analyse <- function(db, force=FALSE, verbose=FALSE) {
   res <- dbSendQuery(db, "SELECT source, id, `file`, `type`, Duration FROM `audioblast`.`recordings`")
   ss <- dbFetch(res)
   dbClearResult(res)
@@ -12,11 +13,11 @@ analyse <- function(db, force=FALSE) {
   for (i in 1:nrow(ss)) {
     if (ss[i, "file"] == "http://bio.acousti.ca/sites/default/files/MASAPO19910526_1812_22g.WAV") next()
     if (ss[i, "file"] == "http://bio.acousti.ca/sites/default/files/MASAPO19910526_1121.WAV") next()
-    print(ss[i, "file"])
+    if (verbose) {print(ss[i, "file"]);}
     tmp <- paste0(tempfile(),".",file_ext(ss[i, "file"]))
-    print("TDSC")
+    if (verbose) {print("TDSC");}
     a_tdsc(db, ss[[i, "source"]], ss[[i, "id"]], ss[[i, "file"]], ss[[i, "type"]], ss[[i, "Duration"]], tmp, force)
-    print("bedoya")
+    if (verbose) {print("bedoya");}
     a_bedoya(db, ss[[i, "source"]], ss[[i, "id"]], ss[[i, "file"]], ss[[i, "type"]], ss[[i, "Duration"]], tmp, force)
     unlink(tmp)
   }
