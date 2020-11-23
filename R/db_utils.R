1585cf564deb9cc4977a96ad3344da541b7a3f96
Ed Baker <edwbaker@gmail.com>
2020-11-23 17:36:52 +0000

    Better escaping of db utils.

--- R/db_utils.R
+++ R/db_utils.R
@@ -1,12 +1,12 @@
 deleteAnalysis <- function(db, table, source, id){
     dbExecute(
       db,
-      paste0("DELETE FROM `",table,"` WHERE source = '",source,"' AND id=",id)
+      paste0("DELETE FROM `",table,"` WHERE source = '",source,"' AND id=`",id,"`")
       )
 }
 
 analysedRowCount <- function(db, table, source, id){
-    sql <- paste0("SELECT COUNT(*) FROM `", table, "` WHERE `source` = '",source,"' AND id=",id)
+    sql <- paste0("SELECT COUNT(*) FROM `", table, "` WHERE `source` = '",source,"' AND id=`",id,"`")
     res <- dbSendQuery(db, sql)
     a <- dbFetch(res)[[1,1]]
     dbClearResult(res)
@@ -14,7 +14,7 @@ analysedRowCount <- function(db, table, source, id){
 }
 
 rowAnalysed <- function(db, table, source, id, startTime){
-    sql <- paste0("SELECT `source`, `id` FROM `",table,"` WHERE `source` = '",source,"' AND id=",id," AND startTime = ",startTime)
+    sql <- paste0("SELECT `source`, `id` FROM `",table,"` WHERE `source` = '",source,"' AND id=`",id,"` AND startTime = ",startTime)
     res <- dbSendQuery(db, sql)
     dbFetch(res)
     rc <- dbGetRowCount(res)
