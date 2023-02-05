d7dce7b678ec0a8024fdbe446f3f7f5d4f9f2047
Ed Baker <edwbaker@gmail.com>
2023-02-05 17:38:47 +0000

    recordings_calculated channels

--- R/recordings_calculated.R
+++ R/recordings_calculated.R
@@ -1,5 +1,6 @@
 #' @importFrom cli hash_file_sha256
 #' @importFrom seewave duration
+#' @importFrom av av_media_info
 #' @export
 recordings_calculated <- function(db, source, id, file, type, duration, tmp, force=FALSE, verbose=FALSE) {
   sql = paste0("SELECT * FROM `recordings-calculated` WHERE `source`='", source, "' AND `id`='", id, "' AND `hash` IS NOT NULL")
@@ -26,6 +27,20 @@ recordings_calculated <- function(db, source, id, file, type, duration, tmp, for
     d <- duration(w)
     print(paste("Duration is: ", duration))
     sql = paste0("INSERT INTO `recordings-calculated` (`source`, `id`, `duration`) VALUES('", source, "', '", id, "', '", duration, "') ON DUPLICATE KEY UPDATE `duration` = '", duration, "';")
+    dbExecute(db, sql)
+  }
+
+
+  sql = paste0("SELECT * FROM `recordings-calculated` WHERE `source`='", source, "' AND `id`='", id, "' AND `channels` IS NOT NULL")
+  res <- dbSendQuery(db, sql)
+  dbFetch(res)
+  if (dbGetRowCount(res) == 1) {
+    print("Alredy calculated channels")
+  } else {
+    dl_file(file, tmp)
+    channels <- av_media_info(file)$audio[['channels']]
+    print(paste("Channels: ", channels))
+    sql = paste0("INSERT INTO `recordings-calculated` (`source`, `id`, `channels`) VALUES('", source, "', '", id, "', '", channels, "') ON DUPLICATE KEY UPDATE `channels` = '", channels, "';")
     dbExecute(db, sql)
   }
 }
