28ceac4ffa3535a461dc6f68853dc1e8f440ad18
Ed Baker <edwbaker@gmail.com>
2023-02-20 20:51:53 +0000

    Comparison!

--- R/analyse.R
+++ R/analyse.R
@@ -42,7 +42,7 @@ analyse <- function(db, mode="local", source="unp", verbose=FALSE, force=FALSE, 
           soundscapes_by_second(db, ss[[i, "source"]], ss[[i, "id"]], ss[[i, "file"]], ss[[i, "type"]], as.numeric(ss[[i, "Duration"]]), tmp, force, verbose)
           deleteToDo(db, ss[[i, "source"]], ss[[i, "id"]], ss[[i, "task"]], process_id)
         }
-        if (mode="web") {
+        if (mode=="web") {
           unlink(tmp)
         }
       }
